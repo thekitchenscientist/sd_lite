1348031a96242be95633651ac64856edacb6ff61
thekitchenscientist <10332829+thekitchenscientist@users.noreply.github.com>
2022-12-30 20:58:25 +0000

    if PNG add image metadata to file

--- CLI.py
+++ CLI.py
@@ -3,23 +3,27 @@ import functions
 import config as config
 from argparse import ArgumentParser
 import os
-
+import importlib
 
 
 def main(args):
 
 	#load config if given
-	if args.config != None:	
+	if args.config is not None:	
 		if os.path.exists(functions.ROOT_DIR+"/prompts/"+args.config+".py"):
 			from prompts import config as config
+	else:
+		import config as config
+
 	
-	#load prompt_list
-	prompt_list = ["cheese and wine","chickens firing guns at polar bears"]
-		
-	if os.path.exists(args.prompts):
-		prompt_list = open(args.prompts)
-	elif os.path.exists(functions.ROOT_DIR+"/prompts/"+args.prompts):
-		prompt_list = open(functions.ROOT_DIR+"/prompts/"+args.prompts)
+	#load prompt_list if given
+	prompt_list = ["cheese and wine","chickens firing guns","polar bear cubs on an iceberg"]
+	
+	if args.prompts is not None:		
+		if os.path.exists(args.prompts):
+			prompt_list = open(args.prompts)
+		elif os.path.exists(functions.ROOT_DIR+"/prompts/"+args.prompts+".txt"):
+			prompt_list = open(functions.ROOT_DIR+"/prompts/"+args.prompts+".txt")
 
 
 	#parse prompt_list
@@ -36,10 +40,10 @@ def main(args):
 		functions.inference(prompt=prompt, anti_prompt=anti_prompt, n_images = config.IMAGE_COUNT, guidance = config.IMAGE_CFG, steps = config.IMAGE_STEPS, width= config.IMAGE_WIDTH, height= config.IMAGE_HEIGHT, seed= config.IMAGE_SEED)
 
 
-#load supplied data
+#parse supplied data
 parser = ArgumentParser()
-parser.add_argument("--prompts", type=str, default="prompt_list.txt", help="prompt file")
-parser.add_argument("--config", type=str, default="config", help="alternative configuration file")
+parser.add_argument("--prompts", type=str, default=None, help="txt prompt file name (and URL if not .\prompts). One line per prompt, use | to add anti-prompts")
+parser.add_argument("--config", type=str, default=None, help="(None|config) alternative configuration file (in .\prompts)")
 args = parser.parse_args()
 main(args)
 
--- functions.py
+++ functions.py
@@ -1,27 +1,41 @@
 ﻿from diffusers import StableDiffusionPipeline, DiffusionPipeline, DPMSolverMultistepScheduler
 import torch
-from PIL import Image
+from PIL import PngImagePlugin, Image
 import random
 import config
 import uuid
 import sqlite3
 import os
 
-ROOT_DIR = os.path.realpath(os.path.join(os.path.dirname(__file__)))
 
 loaded_pipe = config.loaded_pipe
 
-if not os.path.isdir(config.IMAGE_INPUT):
-    os.mkdir(config.IMAGE_INPUT)
+# ensure correct folders exist and are used
+ROOT_DIR = os.path.realpath(os.path.join(os.path.dirname(__file__)))
 
-if not os.path.isdir(config.IMAGE_OUTPUT):
-    os.mkdir(config.IMAGE_OUTPUT)
-    
-if not os.path.isdir(config.PROMPT_FOLDER):
-    os.mkdir(config.PROMPT_FOLDER)
+if config.IMAGE_INPUT_FOLDER is not None:
+    if not os.path.isdir(config.IMAGE_INPUT_FOLDER):
+        os.mkdir(config.IMAGE_INPUT_FOLDER)
+    IMAGE_INPUT_FOLDER = config.IMAGE_INPUT_FOLDER
+else:
+    if not os.path.isdir(ROOT_DIR+"/inputs"):
+        os.mkdir(ROOT_DIR+"/inputs")
+    IMAGE_INPUT_FOLDER = ROOT_DIR+"/inputs"
+
+if config.IMAGE_OUTPUT_FOLDER is not None:
+    if not os.path.isdir(config.IMAGE_OUTPUT_FOLDER):
+        os.mkdir(config.IMAGE_OUTPUT_FOLDER)
+    IMAGE_OUTPUT_FOLDER = config.IMAGE_OUTPUT_FOLDER
+else:
+    if not os.path.isdir(ROOT_DIR+"/outputs"):
+        os.mkdir(ROOT_DIR+"/outputs")
+    IMAGE_OUTPUT_FOLDER = ROOT_DIR+"/outputs"
+
+if not os.path.isdir(ROOT_DIR+"/prompts"):
+    os.mkdir(ROOT_DIR+"/prompts")
 
 
-
+# assign unique ID's to each configuration chosen
 def generate_session_UUID():
     config.session_UUID = str(uuid.uuid4())
     #save the config data to history.db
@@ -37,7 +51,7 @@ def load_txt2img_pipe(loaded_pipe):
 
         scheduler = DPMSolverMultistepScheduler.from_pretrained(config.MODEL_ID, subfolder="scheduler")
         
-        print("Loading txt2img model into memory... This may take 5 minutes.")
+        print("Loading txt2img model into memory... This may take 5 minutes depending on available RAM.")
         
         config.txt2img_pipe = StableDiffusionPipeline.from_pretrained(
               config.MODEL_ID,
@@ -54,7 +68,7 @@ def load_txt2img_pipe(loaded_pipe):
         if config.MEMORY_EFFICIENT_ATTENTION:
             config.txt2img_pipe.enable_xformers_memory_efficient_attention()
             
-        print("txt2img model loaded!")
+        print("txt2img model loaded")
 
 if loaded_pipe == None:
     load_txt2img_pipe(loaded_pipe)
@@ -76,9 +90,11 @@ def text_to_image(prompt, anti_prompt, n_images,  guidance, steps, width, height
 
     output_name = str(uuid.uuid4())
     result = []
+    file_metadata = []
     
     for settings in range(-1,2):
 
+        # setup image properties
         temp_steps = steps + 1 * settings * config.IMAGE_STEPS_OFFSET
         temp_guidance = guidance - 1 * settings * config.IMAGE_CFG_OFFSET
         
@@ -97,17 +113,31 @@ def text_to_image(prompt, anti_prompt, n_images,  guidance, steps, width, height
           height = height,
           generator = generator).images
 
-        result.append(temp_result[0])
-        #print(settings, result)
+        result.extend(temp_result)
+        
+        # record image metadata
+        temp_dict = {}
+        temp_dict["prompt"] = temp_prompt
+        temp_dict["anti_prompt"] = anti_prompt
+        temp_dict["steps"] = str(temp_steps)
+        temp_dict["CFG"] = str(temp_guidance)
+        temp_dict["seed"] = str(seed)
+        temp_dict["n_images"] = str(n_images)
 
+        file_metadata.append(temp_dict)
+
+    # save image to folder
     try:
         for i, image in enumerate(result):
-            #print(i, image)
-            image.save( config.IMAGE_OUTPUT+'/'+output_name+'_'+str(i)+'.'+config.IMAGE_FORMAT, config.IMAGE_FORMAT)
-            #result[i] = image.resize((256,256), Image.LANCZOS)
+            if config.SAVE_METADATA_TO_IMAGE and config.IMAGE_FORMAT == "PNG":
+                metadata = PngImagePlugin.PngInfo()
+                for key, value in file_metadata[i].items():
+                    if isinstance(key, str) and isinstance(value, str):
+                        metadata.add_text(key, value)
+
+            image.save( IMAGE_OUTPUT_FOLDER+'/'+output_name+'_'+str(i)+'.'+config.IMAGE_FORMAT, config.IMAGE_FORMAT, pnginfo=(metadata if config.SAVE_METADATA_TO_IMAGE and config.IMAGE_FORMAT == "PNG" else None))
     except:
         print( "Error saving image" +output_name+'_'+str(i)+'.png')
         
-    #resize images
-
+ 
     return result
